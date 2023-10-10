b0f3ed0e3775f0723e87cc91296dcd2def15ae45
Михаил <88964539+9i5BCrUcNX5NmT@users.noreply.github.com>
2023-10-10 22:13:07 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub mod main_logic {
 
     use crate::hash::hash_of_num;
 
-    pub fn compute_result(null: usize, find: usize) -> Vec<(usize, String)> {
+    pub fn compute_result(null: usize, first: usize) -> Vec<(usize, String)> {
         let counter = Arc::new(Mutex::new(0));
         let pool = ThreadPool::new(num_cpus::get()); // ThreadPool by number of cores
         let (tx, rx) = channel();
@@ -15,31 +15,31 @@ pub mod main_logic {
         let end_str = (0..null).map(|_| '0').collect::<String>(); // generate String to check hashes
         let mut num = 0; // first number for compute hash
 
-        while need_to_contunue(find, Arc::clone(&counter)) {
+        while need_to_contunue(first, Arc::clone(&counter)) {
             hash_of_num_in_thread(
                 &pool,
                 num,
                 end_str.clone(),
-                find,
+                first,
                 tx.clone(),
                 Arc::clone(&counter),
             );
             num += 1;
         }
-        rx.iter().take(find).collect::<Vec<(usize, String)>>()
+        rx.iter().take(first).collect::<Vec<(usize, String)>>()
     }
 
     fn hash_of_num_in_thread(
         pool: &ThreadPool,
         num: usize,
         end_str: String,
-        find: usize,
+        first: usize,
         tx: Sender<(usize, String)>,
         counter: Arc<Mutex<usize>>,
     ) {
         // start new thread
         pool.execute(move || {
-            if need_to_contunue(find, Arc::clone(&counter)) {
+            if need_to_contunue(first, Arc::clone(&counter)) {
                 let hash = hash_of_num(num);
                 if hash.ends_with(end_str.as_str()) {
                     // hash condition check
@@ -50,9 +50,9 @@ pub mod main_logic {
         });
     }
 
-    fn need_to_contunue(find: usize, counter: Arc<Mutex<usize>>) -> bool {
+    fn need_to_contunue(first: usize, counter: Arc<Mutex<usize>>) -> bool {
         // check COUNT for completed tasks
-        *counter.lock().unwrap() != find
+        *counter.lock().unwrap() != first
     }
 }
 
